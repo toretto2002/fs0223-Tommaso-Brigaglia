dbdb363de27b247339c86a5e27eb0a88b6a6ddd4
toretto2002 <tommaso.brigaglia@gmail.com>
2023-04-11 17:03:24 +0200

    esercizi

--- m1/s3/g1/esercizio pomeriggio/js/D6.js
+++ m1/s3/g1/esercizio pomeriggio/js/D6.js
@@ -11,6 +11,13 @@ REGOLE
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
 */
 
+
+function area(l1,l2){
+    return l1*l2
+}
+
+console.log(area(2,4))
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 2
@@ -21,6 +28,16 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazySum(p1,p2){
+    if(p1===p2){
+        return (p1+p2)*3
+    }else{
+        return p1+p2
+    }
+}
+
+console.log(crazySum(2,2));
+
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
@@ -28,12 +45,35 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazyDiff(p1){
+    if(p1 > 19){
+        return (p1-19)*3
+    }else{
+        return Math.abs(p1-19)
+    }
+}
+
+console.log(crazyDiff(17))
+
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function boundary(n){
+    if(n >= 20 && n <= 100){
+        return true
+    }else if(n == 400){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(boundary(28));
+
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -43,6 +83,18 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function epify(str){
+    if(str.indexOf('EPICODE') == -1){
+        return 'EPICODE'+ str
+    }else{
+        return str
+    }
+}
+
+console.log(epify('R BENVENUTO'));
+
+
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
@@ -50,11 +102,39 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function check3and7(p){
+    if(p % 3 == 0){
+        return 'il numero è divisibile per 3'
+    }else if(p % 7 == 0){
+        return 'il numero è divisibile per 7'
+    }else{
+        return 'il numero non è divisibile ne per tre ne per sette'
+    }
+}
+
+console.log(check3and7(56));
+
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function reverseString(str){
+  
+    let j = 0;
+    let reversed = '';
+
+    for(let i = str.length - 1; i >= 0; i--){
+         
+        reversed += str[i];
+        
+    }
+
+    return reversed;
+}
+
+console.log(reverseString('EPICoDe'))
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -63,6 +143,22 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function upperFirst(str){
+
+    str[0] = str[0].toUpperCase();
+
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] == ' '){
+            str[i+1] = str[i+1].toUpperCase();
+        }
+    }
+    
+    return str;
+}
+
+console.log(upperFirst('ciao mi chiamo tommaso'));
+
+
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
@@ -70,8 +166,33 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function cutString(str){
+
+    let newstr = '';
+    for(let i = 1; i < str.length - 1; i++){
+        newstr += str[i]
+    }
+
+    return newstr
+}
+
+console.log(cutString('roccioso'));
+
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function giveMeRandom(n){
+    let arr = [];
+    let i = 0;
+    while(i < n){
+        arr[i] = Math.random()*10
+        i++
+    }
+
+    return arr
+}
+
+console.log(giveMeRandom(5))
