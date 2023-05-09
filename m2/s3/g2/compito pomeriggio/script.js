8d2dc500298afd2f9abc9c766830ca5f981702d9
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-05-09 15:37:16 +0200

    compiti

---
+++ m2/s3/g2/compito pomeriggio/script.js
@@ -0,0 +1,41 @@
+let nameField = document.getElementById("name-field");
+
+let saveButton = document.getElementById("save-button");
+let deleteButton = document.getElementById("delete-button");
+
+let toFillField = document.getElementsByTagName("p")[0];
+
+const fillField = function () {
+  if (localStorage.getItem("name")) {
+    toFillField.innerText = localStorage.getItem("name");
+  }
+  nameField.focus();
+};
+
+saveButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  localStorage.setItem("name", nameField.value);
+
+  fillField();
+});
+
+deleteButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  nameField.value = "";
+  nameField.focus();
+});
+
+let timerField = document.getElementById("timer");
+
+let count = 0;
+
+setInterval(function () {
+  timerField.innerText = count;
+  count += 1;
+}, 1000);
+
+window.onload = function () {
+  fillField();
+};
