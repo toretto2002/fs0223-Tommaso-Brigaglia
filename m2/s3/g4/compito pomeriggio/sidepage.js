d5dc8f3c640165f4ac90abb169f7670d6fd352e4
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-05-11 20:26:27 +0200

    background

--- m2/s3/g4/compito pomeriggio/sidepage.js
+++ m2/s3/g4/compito pomeriggio/sidepage.js
@@ -1,8 +1,9 @@
-const URL_IMG = "https://www.pexels.com/v1/photos/";
+const URL_IMG = "https://api.pexels.com/v1/photos/";
 const APIKEY = "bWR8cNciAR8VqVCfjGDBVpWA1pxj7B4rXvp0cpr5bIDX6m7P1JhIgZJQ";
 
 const getSingleImage = function () {
   let idFromSearch = new URLSearchParams(window.location.search).get("imageId");
+  console.log(idFromSearch);
   fetch(URL_IMG + idFromSearch, {
     headers: {
       Authorization: APIKEY,
@@ -15,5 +16,33 @@ const getSingleImage = function () {
         throw new Error("Couldn't find");
       }
     })
-    .then((photo) => {});
+    .then((photo) => {
+      console.log(photo);
+      let imageContainer = document.getElementById("image-container");
+      let newCol = document.createElement("div");
+      newCol.classList.add("w-auto");
+
+      newCol.innerHTML = `
+        <div class="card mt-5">
+            <img src="${photo.src.original}" class="card-img-top" alt="..." />
+            <div class="card-body d-flex flex-column align-item-center">
+                <a class="w-auto text-align-center" href="${photo.photographer_url}">${photo.photographer}</a>
+                <a class="w-auto text-align-center" href="index.html">Torna Indietro</a>
+            </div>
+        </div>`;
+
+      imageContainer.appendChild(newCol);
+      return photo;
+    })
+    .then((photo) => changeBackground(photo.avg_color))
+    .catch((err) => console.log(err));
+};
+
+const changeBackground = function (color) {
+  let body = document.getElementsByTagName("body")[0];
+  body.style.backgroundColor = color;
+};
+
+window.onload = () => {
+  getSingleImage();
 };
