bd29665f51c25d6d68fa3b0fde26d6c80a583dc7
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-05-30 14:16:29 +0200

    nn

---
+++ m3/s2/g2/lezione mattino/appunti/src/app/attr/attr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attr',
+  templateUrl: './attr.component.html',
+  styleUrls: ['./attr.component.scss'],
+})
+export class AttrComponent {
+  images: string[] = ['assets/'];
+}
