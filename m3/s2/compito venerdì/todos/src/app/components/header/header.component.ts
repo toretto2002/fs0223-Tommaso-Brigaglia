be9c2e4b0b23905680de5a9609c4a4e5a3224368
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-06-04 20:41:21 +0200

    prove

--- m3/s2/compito venerdì/todos/src/app/components/header/header.component.ts
+++ m3/s2/compito venerdì/todos/src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { visitAll } from '@angular/compiler';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  todosPage: boolean = true;
+  @ViewChild('todosLink') todosLink: any;
+  @ViewChild('completedLink') completedLink: any;
 
+  toCompleted() {
+    if (this.todosPage) {
+      this.todosLink.nativeElement.classList.remove('current-page');
+      this.completedLink.nativeElement.classList.add('current-page');
+      this.todosPage = false;
+    } else {
+      this.todosLink.nativeElement.classList.add('current-page');
+      this.completedLink.nativeElement.classList.remove('current-page');
+      this.todosPage = true;
+    }
+  }
 }
