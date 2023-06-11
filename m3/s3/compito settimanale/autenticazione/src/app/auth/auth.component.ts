d961244d8faed7019f3f175ee0c9bd2385ac565d
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2023-06-11 18:31:42 +0200

    finito

--- m3/s3/compito settimanale/autenticazione/src/app/auth/auth.component.ts
+++ m3/s3/compito settimanale/autenticazione/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { IAuthData } from '../interfaces/iauth-data';
 
 @Component({
   selector: 'app-auth',
--- m3/s3/compito settimanale/autenticazione/src/app/auth/register/register.component.ts
+++ m3/s3/compito settimanale/autenticazione/src/app/auth/register/register.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ISignUpData } from 'src/app/interfaces/isign-up-data';
 import { AuthService } from '../auth.service';
+import { IAuthData } from 'src/app/interfaces/iauth-data';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +10,11 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  currentUser!: IAuthData;
+  isLoggedIn: boolean = false;
+
+  @ViewChild('f', { static: true }) form!: NgForm;
+
   constructor(private AuthSvc: AuthService) {}
   data: ISignUpData = {
     email: '',
@@ -15,6 +22,11 @@ export class RegisterComponent {
     name: '',
     surname: '',
   };
+
+  ngOnInit() {
+    this.fillFields();
+  }
+
   register(form: any) {
     console.log(form);
     this.data = form.form.value;
@@ -22,5 +34,31 @@ export class RegisterComponent {
     this.AuthSvc.signUp(this.data).subscribe((accessData) => {
       alert(accessData.user.name);
     });
+  }
+
+  fillFields() {
+    let jsonUser: string | null = localStorage.getItem('user');
+
+    if (jsonUser) {
+      this.currentUser = JSON.parse(<string>jsonUser);
+      this.isLoggedIn = true;
+      console.log(this.currentUser);
+      console.log(this.form);
+      console.log(this.currentUser.user.email);
+
+      let datiDaInserire = {
+        email: this.currentUser.user.email,
+        name: this.currentUser.user.name,
+        surname: this.currentUser.user.surname,
+      };
+
+      console.log(datiDaInserire);
+
+      this.form.form.patchValue(datiDaInserire);
+
+      console.log();
+    } else {
+      console.log('nessun utente loggato');
+    }
   }
 }
